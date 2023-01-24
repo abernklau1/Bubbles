94cb24a777ea4452f8f130d01c19bd398dd0a857
Andrew Bernklau <andrewbernklau@hotmail.com>
2023-01-23 22:37:35 -0700

    Initialized backend and connected it to front end

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 5001;
+
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json(JSON.stringify({ message: "Hello from the server!" }));
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+})
